b31e7b38f822b22a60ca190249c540bee7bd58e2
hiteshkushwah712@gmail.com <hiteshkushwah712@gmail.com>
2025-05-13 12:36:17 +0530

    this is the new code of object and some different that we learn in this.. ok..

--- normalfunction.js
+++ normalfunction.js
@@ -1,64 +1,155 @@
-// // // // // // function greet(name){
+// // // // // // // // // // // // // // // // // // // function greet(name){
 
-// // // // // //     return "hello this is " + name + "!";
-// // // // // //     }
-// // // // // //     console.log(greet("hitesh"))
+// // // // // // // // // // // // // // // // // // //     return "hello this is " + name + "!";
+// // // // // // // // // // // // // // // // // // //     }
+// // // // // // // // // // // // // // // // // // //     console.log(greet("hitesh"))
 
     
-// // // // // function greet(x ,y){
-// // // // //     return x * y;
-// // // // // }
+// // // // // // // // // // // // // // // // // // function greet(x ,y){
+// // // // // // // // // // // // // // // // // //     return x * y;
+// // // // // // // // // // // // // // // // // // }
 
-// // // // // console.log(greet(2,5))
-
-
-
-// // // // function  seconds(min){
-// // // //     return  min * 60
-// // // // }
-// // // // console.log(seconds(3))
-// // // function rectangle(a, b){
-// // //     return a * b;
-// // // }
-// // // console.log(2,4)
+// // // // // // // // // // // // // // // // // // console.log(greet(2,5))
 
 
-// // function evenorodd(num){
-// //     if (num % 2 == 0){
-// //         return true;
+
+// // // // // // // // // // // // // // // // // function  seconds(min){
+// // // // // // // // // // // // // // // // //     return  min * 60
+// // // // // // // // // // // // // // // // // }
+// // // // // // // // // // // // // // // // // console.log(seconds(3))
+// // // // // // // // // // // // // // // // function rectangle(a, b){
+// // // // // // // // // // // // // // // //     return a * b;
+// // // // // // // // // // // // // // // // }
+// // // // // // // // // // // // // // // // console.log(2,4)
+
+
+// // // // // // // // // // // // // // // function evenorodd(num){
+// // // // // // // // // // // // // // //     if (num % 2 == 0){
+// // // // // // // // // // // // // // //         return true;
         
-// //     } else {
-// //         return false
-// //     }
-// // }
-// // console.log(evenorodd(4))
+// // // // // // // // // // // // // // //     } else {
+// // // // // // // // // // // // // // //         return false
+// // // // // // // // // // // // // // //     }
+// // // // // // // // // // // // // // // }
+// // // // // // // // // // // // // // // console.log(evenorodd(4))
 
 
 
 
 
-// function greet(name){
-//     return "hii my name is " + name + "and i know you know me"
-// }
-// console.log(greet("hitesh kushwah"))
+// // // // // // // // // // // // // // function greet(name){
+// // // // // // // // // // // // // //     return "hii my name is " + name + "and i know you know me"
+// // // // // // // // // // // // // // }
+// // // // // // // // // // // // // // console.log(greet("hitesh kushwah"))
 
 
 
 
-// function marks(m1, m2 , m3){
-//     return (m1 + m2 + m3 )/3 
-// }
-// console.log(marks(1,3,4))
+// // // // // // // // // // // // // // function marks(m1, m2 , m3){
+// // // // // // // // // // // // // //     return (m1 + m2 + m3 )/3 
+// // // // // // // // // // // // // // }
+// // // // // // // // // // // // // // console.log(marks(1,3,4))
 
 
-// function hitesh(age){
-//     if(age > 18){
-//         return true;
+// // // // // // // // // // // // // // function hitesh(age){
+// // // // // // // // // // // // // //     if(age > 18){
+// // // // // // // // // // // // // //         return true;
 
-//     } else {
-//         return false;
+// // // // // // // // // // // // // //     } else {
+// // // // // // // // // // // // // //         return false;
 
-//     }
-// } 
-// console.log(hitesh(14))
+// // // // // // // // // // // // // //     }
+// // // // // // // // // // // // // // } 
+// // // // // // // // // // // // // // console.log(hitesh(14))
+
+
+
+
+// // // // // // // // // // // // // let firstName  = "htiesh"
+// // // // // // // // // // // // // let secondName = "kushwah"
+
+// // // // // // // // // // // // // console.log(firstName, secondName)
+
+
+// // // // // // // // // // // // const firstNumber = 23;
+// // // // // // // // // // // // const secondNumber = 25;
+// // // // // // // // // // // // console.log(firstNumber)
+// // // // // // // // // // // // console.log(typeof secondNumber)
+
+
+
+// // // // // // // // // // // const istrue = true;
+// // // // // // // // // // // const isfalse = false;
+// // // // // // // // // // // console.log(istrue)
+// // // // // // // // // // // console.log(typeof isfalse)
+
+
+// // // // // // // // // // // let score ;
+// // // // // // // // // // // console.log(score)
+// // // // // // // // // // // console.log(typeof score)
+
+// // // // // // // // // // let result    = null;
+// // // // // // // // // // console.log(result)
+// // // // // // // // // // console.log(typeof result)
+
+// // // // // // // // // let number = 345236145252624517;
+// // // // // // // // // console.log(number)
+// // // // // // // // // console.log(typeof  number)
+
+// // // // // // // // let person = {
+// // // // // // // //     name:  "hitesh kushwah",
+// // // // // // // //     age: 23,
+// // // // // // // //     isEmployed :  true
+
+
+// // // // // // // // }
+
+// // // // // // // // console.log([person])
+
+
+
+// // // // // // // let fruits = ["apple", "banana", "cherry"]
+// // // // // // // console.log(fruits)
+// // // // // // // console.log(typeof fruits);
+
+
+
+// // // // function greet() {
+// // // //   return "Hello!";
+// // // // }
+// // // // console.log(greet());         // Output: Hello!
+// // // // console.log(typeof greet);    // Output: function
+
+
+// // // // // function greet(a,e){
+// // // // //     return a + e
+// // // // // }
+// // // // // console.log(greet(2,6))
+// // // // // console.log(typeof greet())
+
+
+
+// // // let obj = { name: "Sourav", age: 23 };
+
+// // // // Using Dot Notation
+// // // console.log(obj['name']);
+
+// // // // Using Bracket Notation
+// // // console.log(obj.age);
+
+
+// // let obj = new Object()
+// // obj.name = "hitesh"
+// // obj.age = 14
+// // obj.role = "ai engineer"
+// // console.log(obj)
+
+
+// let obj  =  new Object()
+// obj.name = "hitesh"
+// console.log(obj)
+
+// obj.name = "deepesh"
+// console.log(obj)
+
 
